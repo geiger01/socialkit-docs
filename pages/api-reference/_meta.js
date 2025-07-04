0a7bf5486340b5f92db146f74309674248a69aa3
geiger01 <tantangeiger@gmail.com>
2025-07-05 00:24:56 +0300

    stats

--- pages/api-reference/_meta.js
+++ pages/api-reference/_meta.js
@@ -2,5 +2,6 @@ export default {
 	index: 'Overview',
 	'youtube-summarize-api': 'YouTube Summary API',
 	'youtube-transcript-api': 'YouTube Transcript API',
+	'youtube-stats-api': 'YouTube Stats API',
 	errors: 'Errors',
 };
