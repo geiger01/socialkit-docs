32c94644d68473b6323461e174f0ae5371a4d4bf
geiger01 <tantangeiger@gmail.com>
2025-08-16 14:55:41 +0300

    comments

--- pages/api-reference/_meta.js
+++ pages/api-reference/_meta.js
@@ -3,5 +3,6 @@ export default {
 	'youtube-summarize-api': 'YouTube Summary API',
 	'youtube-transcript-api': 'YouTube Transcript API',
 	'youtube-stats-api': 'YouTube Stats API',
+	'youtube-comments-api': 'YouTube Comments API',
 	errors: 'Errors',
 };
