f5fec794877b71a9c2e338c901ea1a0728704605
geiger01 <tantangeiger@gmail.com>
2025-10-09 22:59:23 +0300

    tiktok support

--- pages/api-reference/_meta.js
+++ pages/api-reference/_meta.js
@@ -4,6 +4,8 @@ export default {
 	'youtube-transcript-api': 'YouTube Transcript API',
 	'youtube-stats-api': 'YouTube Stats API',
 	'youtube-comments-api': 'YouTube Comments API',
+	'tiktok-summarize-api': 'TikTok Summary API',
+	'tiktok-transcript-api': 'TikTok Transcript API',
 	'tiktok-stats-api': 'TikTok Stats API',
 	'tiktok-comments-api': 'TikTok Comments API',
 	errors: 'Errors',
